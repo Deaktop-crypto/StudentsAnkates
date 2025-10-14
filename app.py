2eb1d6f70c9966ff4994114c2ea08e2ba89d084e
全人類の劣化 <somesame2525@gmail.com>
2025-10-14 20:49:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,66 @@
 import streamlit as st
 import pandas as pd
 import datetime
+import calender
 import os
 
-# CSVファイル名
 DATA_FILE = "data.csv"
 
-# データファイルがない場合は作成
 if not os.path.exists(DATA_FILE):
-    df = pd.DataFrame(columns=["出席番号","日付", "時間", "理解度"])
+    df = pd.DataFrame(columns=["出席番号","週","月", "曜日", "時間",])
     df.to_csv(DATA_FILE, index=False, encoding="utf-8-sig")               
 
-# タイトル
-st.title(" 授業理解度アンケート ")
+st.title(" 苦手な授業調査 ")
 
-# 今日の日付
 today = datetime.date.today()
+first_day = today.replace(day=1)
 
-# 選択フォーム
+week_number = (today.day + first_day.weekday()) // 7 + 1
+
+month = today.month
+
 st.header("出席番号")
 name = st.text_input("出席番号")
 
-st.subheader(" 時間を選択してください")
-period = st.selectbox("時間を選んでください", [f"{i}時間目" for i in range(1, 7)])
+st.subheader("曜日を選択してください")
+youbi = ['月曜日','火曜日','水曜日','木曜日','金曜日']
+period = st.selectbox("", youbi, index = None)
 
-st.subheader(f"{period} の理解度を入力してください")
-understanding = st.slider("理解度（1: 難しかった 〜 5: よく理解できた）", 1, 5, 3)
+time = ['一時間目','二時間目','三時間目','四時間目','五時間目','六時間目']
+st.subheader(f"{period} の時間を選択してください")
+understanding = st.selectbox("", time, index = None)
 
-# 送信ボタン
 if st.button("送信"):
     if not name:
         st.write("入力されていない項目があります")
     else:
         new_data = pd.DataFrame({
         "出席番号":[name],
-        "日付": [today],
-        "時間": [period],
-        "理解度": [understanding]
+        "週": [weeknumber],
+        "月":[month],
+        "曜日": [youbi],
+        "時間": [understanding]
     })
     df = pd.read_csv(DATA_FILE)
     df = pd.concat([df, new_data], ignore_index=True)
     df.to_csv(DATA_FILE, index=False, encoding="utf-8-sig")
     st.success(" 回答が送信されました。")
 
-# 集計表示
 st.header(" 集計結果")
 df = pd.read_csv(DATA_FILE)
 
-# 日付フィルター
-selected_date = st.date_input("日付を選択", today)
 
-filtered_df = df[df["日付"] == str(selected_date)]
+month_options = ['10','11','12','1','2','3']
+selected_month = st.selectbox("月を選択してください", month_options, index = None)
+week_options = ['1','2','3','4']
+selected_week = st.selectbox("何週目かを選択してください", week_options, index = None)
+
+filtered_df = df[(df["月"] == str(selected_month)) && (["週"] == str(selected_week))]
 
 if filtered_df.empty:
     st.info("この日付のデータはまだありません。")
 else:
-    avg_scores = filtered_df.groupby("時間")["理解度"].mean().reset_index()
-    st.bar_chart(avg_scores.set_index("時間"))
-
+    graph = filterd_df.groupby("曜日")["時間"]
+    st.bar_chart(graph.set_index("曜日"))
 if name == "イチジクのタルト":
         st.dataframe(filtered_df)
-
