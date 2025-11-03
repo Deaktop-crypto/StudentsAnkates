d3a6bd0c3e765d74e3bcf7cb7dd1e27159cb3853
全人類の劣化 <somesame2525@gmail.com>
2025-11-03 17:44:08 +0900

    Reformat app.py with consistent indentation

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ if st.button("送信"):
 
 target_date = datetime.date(2025, 9, 9)
 if date == target_date:
-    selected_date = st.date_input("日付を選択", today)
+    selected_date = st.date_input("日付を選択",)
     filtered_df = df[df["日付"] == str(selected_date)]
     st.dataframe(filtered_df)
+
